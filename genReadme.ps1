f331f0d2533aa3124376923d59bb3516669475d5
nohuto <nohuto@tuta.io>
2026-09-21 15:31:39 +0100

    3 colms

--- genReadme.ps1
+++ genReadme.ps1
@@ -4,11 +4,11 @@ $folders = 'w7', 'w11'
 $lines = @('# Windows icons', '')
 
 foreach ($f in $folders) {
-    $lines += "## $f", '', '| - | - | - | - | - |', '| :---: | :---: | :---: | :---: | :---: |'
+    $lines += "## $f", '', '| - | - | - |', '| :---: | :---: | :---: |'
     $icons = @(dir (Join-Path $PSScriptRoot $f) -Recurse -File -Filter *.ico | sort FullName)
 
-    for ($i = 0; $i -lt $icons.Count; $i += 5) {
-        $cells = for ($j = 0; $j -lt 5; $j++) {
+    for ($i = 0; $i -lt $icons.Count; $i += 3) {
+        $cells = for ($j = 0; $j -lt 3; $j++) {
             if ($i + $j -ge $icons.Count) { ''; continue }
             $icon = $icons[$i + $j]
             $path = $icon.FullName.Substring($PSScriptRoot.Length + 1).Replace('\', '/')

0308c542fbc9a3a4695597f127212b4b80760d22
nohuto <nohuto@tuta.io>
2026-09-21 16:02:46 +0100

    headers

--- genReadme.ps1
+++ genReadme.ps1
@@ -13,8 +13,7 @@ foreach ($f in $folders) {
             $icon = $icons[$i + $j]
             $path = $icon.FullName.Substring($PSScriptRoot.Length + 1).Replace('\', '/')
             $url = ($path.Split('/') | % { [Uri]::EscapeDataString($_) }) -join '/'
-            $name = [Net.WebUtility]::HtmlEncode($icon.Name).Replace('|', '&#124;')
-            "<a href=`"$url`"><img src=`"$url`" width=`"48`" alt=`"$name`"><br><sub>$name</sub></a>"
+            "<img src=`"$url`" width=`"48`">"
         }
         $lines += '| ' + ($cells -join ' | ') + ' |'
     }
